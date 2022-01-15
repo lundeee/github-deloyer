9c703cae22d466c603a241c4fd4ca655f64dbb1d
lundeee <lundeee@gmail.com>
2022-01-15 11:33:17 +0100

    fd

--- config.js
+++ config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  projects = [
+  projects: [
     {
       name: "github-deployer",
       script: "./scripts/update_self.sh"
--- src/services/deploy.js
+++ src/services/deploy.js
@@ -1,4 +1,4 @@
-const config = require('./config.js')
+const config = require('../../config.js')
 
 async function GetLogs() {
   const { spawn } = require('child_process');
