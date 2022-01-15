cbdd16322e1ca6d195ecd6faa468ab9d53461c64
lundeee <lundeee@gmail.com>
2022-01-15 12:14:07 +0100

    d

--- src/services/deploy.js
+++ src/services/deploy.js
@@ -24,7 +24,7 @@ async function Deploy(body) {
   console.log(`Updating ${body.repository.name} to commit: ${body.head_commit.message}`)
   const proj = config.projects.find(x => {x.name === body.repository.name})
   console.log(proj, config.projects)
-  spawn("./scripts/update_self.sh");
+  spawn("scripts/update_self.sh");
   // if (proj) {
   //   // console.log(`Updating ${body.repository.name} to commit: ${body.commits.message}`)
   //   const child = spawn("./scripts/" + proj.script);
