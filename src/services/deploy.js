b5c516a5ef84e05ea66398a8b293b0c87e25ecc2
lundeee <lundeee@gmail.com>
2022-01-16 10:44:55 +0100

    asdf

--- src/services/deploy.js
+++ src/services/deploy.js
@@ -3,26 +3,7 @@ const { spawn } = require('child_process')
 const fs = require('fs')
 
 async function GetLogs() {
-  const { spawn } = require('child_process');
-  if (fs.existsSync(script)) {
-    const child = spawn('../../scripts/test.sh');
-    child.on('exit', function (code, signal) {
-      if(code === 0) {
-        console.log("success")
-      } else {
-        console.log('child process exited with ' +
-        `code ${code} and signal ${signal}`);
-      }
-
-    });
-    child.stdout.on('data', (data) => {
-      console.log(`child stdout:\n${data}`);
-    });
-
-    child.stderr.on('data', (data) => {
-      console.error(`child stderr:\n${data}`);
-    });
-  }
+  return "github-deployer is running."
 }
 
 async function Deploy(body) {
